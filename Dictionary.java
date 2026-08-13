7e1291636bbd61e187d9fba63f80d4db3c8a80cc
Bivudb <bivudb@gmail.com>
2026-08-13 10:17:45 -0700

    Implement file saving functionality in Dictionary
    Added fileSaver method to save dictionary entries to a text file.

--- Dictionary.java
+++ Dictionary.java
@@ -1,14 +1,38 @@
 package condict;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.HashMap;
 
 public class Dictionary {
-	Map<String, Entry> dict = new HashMap<>();
+	HashMap<String, Entry> dict = new HashMap<>();
 
 	String cLang;
 
 	public HashMap<String, Entry> addEntry(String key, Entry entry) {
 		dict.put(key, entry);
+		return dict;
 	}
+	
+
+	public boolean fileSaver(boolean entriesDone) {
+		String sep = File.separator;
+		String home = System.getProperty("user.home");
+		String filePath = home + sep + "Documents" + sep + "Dictionary" + sep + cLang + ".txt";
+		try (PrintWriter writer = new PrintWriter (new FileWriter(filePath))){
+			for (Entry e : dict.values()) {
+				writer.println(e.word + "|" + e.ipa + "|" + e.translation + "|" + e.pos);
+			}
+			System.out.println("Dictionary Saved");
+			return entriesDone;
+		} catch (IOException e) {
+			System.out.println("Could not save");
+			return false;
+		}
+	}
+	
 }
