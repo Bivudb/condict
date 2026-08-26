45d27d47b5cfaf4c378772c4d01d51f6c7dbf3cb
Bivudb <bivudb@gmail.com>
2026-08-25 20:31:35 -0700

    Enhance Entry class with additional attributes
    Added new fields for compound information, description, etymology, and example sentence to the Entry class.

--- Entry.java
+++ Entry.java
@@ -5,14 +5,22 @@ public class Entry {
 	String word;
 	String pos;
 	String ipa;
-
-	public Entry(String word, String ipa, String translation, String pos) {
+	String compoundInfo;
+	String Description;
+	String Etymology;
+	String exampleSentence;
+	
+	Entry(String word, String ipa, String translation, String pos, String compoundInfo, String Etymology, String Description, String exampleSentence) {
 		this.word = word;
 		this.ipa = ipa;
 		this.translation = translation;
 		this.pos = pos;
+		this.compoundInfo = compoundInfo;
+		this.Description = Description;
+		this.Etymology = Etymology;
+		this.exampleSentence = exampleSentence;
 	}
 	public String toString() {
-	    return word + " [" + ipa + "] - " + translation + " (" + pos + ")";
+	    return word + " [" + ipa + "] - " + translation + " (" + pos + ")" + compoundInfo + "(" + Description + ")" + Etymology + "[" + exampleSentence + "]";
 	}
 }
